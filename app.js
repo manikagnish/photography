21c8ccb364c47d74d9fe322df917071e0803a650
manikagnish <manikagnish123@gmail.com>
2021-05-17 06:28:08 +0530

    compressed images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
-import { navbar } from "/navbar.js";
-import { carousel } from "/carousel.js";
-import { workCard } from "/workCard.js";
+import { navbar } from "./navbar.js";
+import { carousel } from "./carousel.js";
+import { workCard } from "./workCard.js";
 
 //NAVBAR
 navbar();
